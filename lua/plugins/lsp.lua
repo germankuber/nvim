e8df6c4994e2de5c928ba6c63e6cbd015428d4e2
Germán Küber <german.kuber@outlook.com>
2024-11-23 20:29:55 +0000

    Added hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,13 +1,32 @@
 return {
-
     {
         "neovim/nvim-lspconfig",
-        lazy = false,
-        dependencies = {
-            "jose-elias-alvarez/null-ls.nvim", "nvim-telescope/telescope.nvim",
-            'andrew-george/telescope-themes'
-        },
-        -- config = function() require "configs.lspconfig" end
-    }
+        config = function()
+            require("lspconfig").rust_analyzer.setup({
+                on_attach = function(client, bufnr)
+                    -- Keymap for hover documentation
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover,
+                                   {buffer = bufnr, silent = true})
 
+                    -- Enable inlay hints
+                    if client.server_capabilities.inlayHintProvider then
+                        vim.lsp.buf.inlay_hint(bufnr, true)
+                    end
+                end,
+                settings = {
+                    ["rust-analyzer"] = {
+                        cargo = {allFeatures = true}, -- Enable all Cargo features
+                        checkOnSave = {command = "clippy"}, -- Use Clippy for linting
+                        procMacro = {enable = true}, -- Enable procedural macros
+                        inlayHints = {
+                            locationLinks = true,
+                            typeHints = true, -- Show type hints
+                            chainingHints = true, -- Show hints for method chaining
+                            parameterHints = true -- Show hints for parameters
+                        }
+                    }
+                }
+            })
+        end
+    }
 }
