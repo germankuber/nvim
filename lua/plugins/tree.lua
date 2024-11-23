5f5ffe80913ba1c3b0ee636208854c7085e22fd7
Germán Küber <german.kuber@outlook.com>
2024-11-23 01:58:17 +0000

    Parse command nvimtreee

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,28 +1,35 @@
 return {
-    {
-      "nvim-tree/nvim-tree.lua",
-      lazy = false, -- Cargar inmediatamente
-      dependencies = { "nvim-tree/nvim-web-devicons" }, -- Iconos opcionales
-      config = function()
-        require("nvim-tree").setup({
-          -- Configuración básica
-          view = {
-            width = 30,
-            side = "left",
-            number = true,
-            relativenumber = true,
-          },
-          filters = {
-            dotfiles = false, -- Mostrar archivos ocultos
-          },
-          git = {
-            enable = true, -- Mostrar iconos de Git
-          },
-          renderer = {
-            group_empty = true, -- Agrupar carpetas vacías
-          },
-        })
-      end,
-    },
-  }
-  
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false, -- Cargar inmediatamente
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- Iconos opcionales
+    config = function()
+      require("nvim-tree").setup({
+        on_attach = function(bufnr)
+          local api = require("nvim-tree.api")
+          local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+
+          -- Mapear <CR> para abrir nodos (archivos o carpetas)
+
+        end,
+        view = {
+          width = 30,
+          side = "left",
+          number = true,
+          relativenumber = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+        git = {
+          enable = true,
+        },
+        renderer = {
+          group_empty = true,
+        },
+      })
+    end,
+  },
+}
