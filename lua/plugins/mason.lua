ad7b0b5bf1c4b10b0b16434e480d069d62784a5c
Germán Küber <german.kuber@outlook.com>
2025-10-31 10:18:58 +0000

    fix(mason): remove csharp_ls from ensure_installed to avoid lspconfig csharp_ls.lua error; recommend OmniSharp instead

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -21,8 +21,8 @@ return {
                     "solidity_ls", -- Solidity
                     "pyright", -- Python
                     "ts_ls", -- TypeScript/JavaScript
-                    -- "omnisharp", -- C# (OmniSharp) [deshabilitado a pedido]
-                    "csharp_ls", -- C# principal
+                    -- "omnisharp", -- C# (OmniSharp)
+                    -- "csharp_ls", -- C# alternative (disabled due to lspconfig issue)
                 },
                 automatic_installation = true -- Automatically install servers
             })
